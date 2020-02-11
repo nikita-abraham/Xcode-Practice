e4b77911775254f1236defd14152acd2d4b3ae89
Nikita Abraham <nikitaabraham.design@gmail.com>
2020-02-10 20:36:55 -0500

    Initial Commit

---
+++ Xcode-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode-Practice
+//
+//  Created by Nikita Abraham on 2/10/20.
+//  Copyright © 2020 Nikita Abraham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
